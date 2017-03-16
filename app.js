6104680c2eb59664afd5e2bc2c538f0c63dd73ed
kunalv508 <kunalv508@gmail.com>
2017-03-16 11:35:41 -0700

    Update app.js

--- app.js
+++ app.js
@@ -8,10 +8,7 @@ console.log(localStorage.getItem("values"));
 var ls = JSON.parse(localStorage.getItem("values"));
 
 if(ls != null){
-  //console.log();
-
   let len = ls.length;
-
   for(let a = 0;a < len; a++ ){
     const li = '<li onclick="changeCss(this)">'+ls[a]+' <button onclick="del(this)">Delete</button></li>';
     ol.innerHTML = ol.innerHTML + li;
@@ -19,39 +16,27 @@ if(ls != null){
     i++;
   }
 }
-//get local storag ends
-
-
 
 addtodo.addEventListener("click", function add() {
-  const inputvalue = iteminput.value;
-  
-  const li = '<li onclick="changeCss(this)">'+inputvalue+' <button onclick="del(this)">Delete</button></li>';
-  ol.innerHTML = ol.innerHTML + li;
-  
-  values[i] = inputvalue;
-   localStorage.setItem("values", JSON.stringify(values));
- //  localStorage.setItem("count.",i);
-    i++;
+	const inputvalue = iteminput.value;
+	const li = '<li onclick="changeCss(this)">'+inputvalue+' <button onclick="del(this)">Delete</button></li>';
+	ol.innerHTML = ol.innerHTML + li;
+	values[i] = inputvalue;
+	localStorage.setItem("values", JSON.stringify(values));
+i++;
 });
-//to delete li
+
+//deleting an li
 function del(e){
   let delItem = e.parentElement.textContent.replace(" Delete", "");
   let rm = JSON.parse(localStorage.getItem("values"));
-  //console.log(rm);
   var index = rm.indexOf(delItem);
-  rm.splice(index, 1);
+  rm.splice(index, 1);//
   localStorage.setItem("values", JSON.stringify(rm));
-  //console.log(rm);
-  //str.replace("Microsoft", "W3Schools");
   e.parentElement.remove();//delete parent li on button click
 }
 
-//to change css of selected li
+//changing css of selected li
 function changeCss(e){
-  e.style.color = "red";
-  e.style.textDecoration = "line-through";
-  
+  e.className += " tddone";
 }
-
-// Check browser support
